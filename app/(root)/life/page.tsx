dc4c25200de2f33aa3c5d0195d60e9f5857b89b1
wood.wu <kilokingw@gmail.com>
2024-06-20 14:27:42 +0800

    Update page.tsx

--- app/(root)/life/page.tsx
+++ app/(root)/life/page.tsx
@@ -42,7 +42,7 @@ const Life = () => {
             return(
               <SwiperSlide key={item.image}>
                 <div className='flex flex-col md:flex-row items-end gap-10'>
-                  <div className='md:w-1/2 '>
+                  <div className='md:w-1/2 w-full '>
                     <Image src={`https://pub-a73f7d8384414e33a55eebf09fd805cd.r2.dev/life/${item.image}`} alt="image" 
                       width={200} height={40} priority={false} 
                       layout="responsive" 
